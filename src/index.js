e5e080f8ba8ec4c8acf219f258d4f9e91bdd9d8a
paulohmoreira <phmoreira37@gmail.com>
2022-12-06 15:45:45 -0300

    Configurado prettier

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./pages/Home";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './pages/Home';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
