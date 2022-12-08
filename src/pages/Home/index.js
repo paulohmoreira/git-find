6b0722cdb2b91992eb6afdf07ead4ba1cd289a9f
paulohmoreira <phmoreira37@gmail.com>
2022-12-08 11:17:31 -0300

    Adicionado background na home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,16 @@
 import { Header } from '../../components/Header';
 
+import background from '../../assets/github.svg';
+
+import './styles.css';
+
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="conteudo">
+        <img src={background} className="background" alt="Imagem de background" />
+      </div>
     </div>
   );
 }
