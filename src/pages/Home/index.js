f15e1594431afab876226a6a7d0269863304dc97
paulohmoreira <phmoreira37@gmail.com>
2022-12-08 14:06:31 -0300

    Adicionado seção profile

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -10,9 +10,25 @@ function App() {
       <Header />
       <div className="conteudo">
         <img src={background} className="background" alt="Imagem de background" />
+
         <div className="info">
           <input name="usuario" placeholder="@username" />
           <button>Buscar</button>
+
+          <div className="perfil">
+            <img
+              src="https://avatars.githubusercontent.com/u/16468584?v=4"
+              alt="Foto de perfil"
+              className="profile"
+            />
+
+            <div>
+              <h3>Paulo Moreira</h3>
+              <span>@paulohmoreira</span>
+              <p>Descrição</p>
+            </div>
+          </div>
+          <hr />
         </div>
       </div>
     </div>
